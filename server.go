d56beef5dc37a6f199d918fee229d3359ec7a3fc
portless-development <112107462+portless-development@users.noreply.github.com>
2022-11-17 19:51:03 +0700

    Update server.go

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World22!")
+		return c.String(http.StatusOK, "Hello, World232!")
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
